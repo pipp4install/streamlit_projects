a7b70dfd671619dd6c638894a3f7110b3d1979bc
Pip <70948349+pippinstall@users.noreply.github.com>
2021-10-12 14:58:04 +0100

    Update app.py

--- hackathon_expenditure/app.py
+++ hackathon_expenditure/app.py
@@ -24,36 +24,36 @@ def load_data(url):
 
 def create_app(df):
     """Displays information about selected features."""
-    authority = st.sidebar.multiselect("Select authority:", df['Authority'])
-    service = st.sidebar.multiselect("Select service:", df.loc[:, df.columns != 'Authority'])
+    authority = st.selectbox('Select authority:', df['Authority'])
+    st.write('You selected:', authority)
 
-    st.header("View and filter table:")
-    cm = sns.light_palette("seagreen", as_cmap=True)
-    st.dataframe(df.style.background_gradient(cmap=cm))
+#     st.header("View and filter table:")
+#     cm = sns.light_palette("seagreen", as_cmap=True)
+#     st.dataframe(df.style.background_gradient(cmap=cm))
 
-    # Main body
-    st.header("View and filter on a map:")
-    st.write("The shade size of the dot represents the expenditure")
+#     # Main body
+#     st.header("View and filter on a map:")
+#     st.write("The shade size of the dot represents the expenditure")
 
-    fig = px.scatter_mapbox(authority,
-                            lat = "Coorindates N",
-                            lon = "Coorindates W",
-                            hover_name = "Authority",
-                            hover_data = service,
-                            zoom = 8,
-                            height = 300,
-                            color = service,
-                            size = service,
-                            color_continuous_scale = px.colors.diverging.Portland, # https://plotly.com/python/builtin-colorscales/
-                            size_max = 9)
-    fig.update_layout(mapbox_style = "stamen-terrain") # open-street-map # stamen-terrain
-    fig.update_layout(margin = {"r":0,"t":0,"l":0,"b":0})
+#     fig = px.scatter_mapbox(authority,
+#                             lat = "Coorindates N",
+#                             lon = "Coorindates W",
+#                             hover_name = "Authority",
+#                             hover_data = service,
+#                             zoom = 8,
+#                             height = 300,
+#                             color = service,
+#                             size = service,
+#                             color_continuous_scale = px.colors.diverging.Portland, # https://plotly.com/python/builtin-colorscales/
+#                             size_max = 9)
+#     fig.update_layout(mapbox_style = "stamen-terrain") # open-street-map # stamen-terrain
+#     fig.update_layout(margin = {"r":0,"t":0,"l":0,"b":0})
 
-    st.plotly_chart(fig, use_container_width = True)
+#     st.plotly_chart(fig, use_container_width = True)
     
-    st.header("View and filter table:")
-    cm = sns.light_palette("seagreen", as_cmap=True)
-    st.dataframe(selected.style.background_gradient(cmap=cm))
+#     st.header("View and filter table:")
+#     cm = sns.light_palette("seagreen", as_cmap=True)
+#     st.dataframe(selected.style.background_gradient(cmap=cm))
 
 
 per_capita = load_data("https://raw.githubusercontent.com/pippinstall/streamlit_projects/main/hackathon_expenditure/per_capita.csv")
